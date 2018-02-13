8d449a7cc415d95cec24ec21ab3648b76f3243b4
Joe Xiong <xzh.cmu@gmail.com>
2018-02-13 04:33:10 -0500

    add json test file
    not finished.
    add some json file to test
    kind of working

--- convert.py
+++ convert.py
@@ -1,10 +1,10 @@
 import cc_dat_utils
-
+import test_json_utils
 #Part 1
 #Use cc_data_utils.make_cc_data_from_dat() to load pfgd_test.dat
 #print the resulting data
 cc_dat = cc_dat_utils.make_cc_data_from_dat("data/pfgd_test.dat")
-print(cc_dat)
+#print(cc_dat)
 #print("data/pfgd_test.dat")
 
 
@@ -18,8 +18,8 @@ input_json_file = "data/test_data.json"
 #Use make_game_library_from_json(json_data) to convert the data to GameLibrary data
 #Print out the resulting GameLibrary data using print_game_library(game_library_data) in test_data.py
 ### End Add Code Here ###
-
-
+game_library = test_json_utils.make_game_library_from_json(input_json_file)
+print("END!!!!!")
 #Part 3
 #Load your custom JSON file
 #Convert JSON data to cc_data
--- test_json_utils.py
+++ test_json_utils.py
@@ -6,6 +6,14 @@ def make_game_library_from_json( json_data ):
     #Initialize a new GameLibrary
     game_library = test_data.GameLibrary()
 
+    #read the json file
+    json_data = open(json_data, 'r').read()
+    decoded_games = json.loads(json_data)
+    print('DECODED:', decoded_games)
+
+    for decoded_game in decoded_games:
+        print (decoded_game)
+
     #Loop through the json_data
         #Create a new Game object from the json_data by reading
         #  title
