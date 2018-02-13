c58ed4174969c9cd4ed772cd48e20edbb499d40b
Joe Xiong <xzh.cmu@gmail.com>
2018-02-13 04:41:34 -0500

    finish decode json file

--- test_json_utils.py
+++ test_json_utils.py
@@ -12,8 +12,14 @@ def make_game_library_from_json( json_data ):
     print('DECODED:', decoded_games)
 
     for decoded_game in decoded_games:
-        print (decoded_game)
+        title = decoded_games[decoded_game]['title']
+        year = decoded_games[decoded_game]['Year']
 
+        platform_name = decoded_games[decoded_game]['platform']['name']
+        platform_launch_year = decoded_games[decoded_game]['platform']['launch year']
+        platform = test_data.Platform(name = platform_name, launch_year=platform_launch_year)
+
+        game_library.games.append(test_data.Game(title=title, year=year, platform=platform))
     #Loop through the json_data
         #Create a new Game object from the json_data by reading
         #  title
@@ -21,5 +27,5 @@ def make_game_library_from_json( json_data ):
         #  platform (which requires reading name and launch_year)
         #Add that Game object to the game_library
     #Return the completed game_library
-
+    print(game_library)
     return game_library
