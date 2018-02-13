cb009697323ef5ee26838e60f03983ef3959b1ba
Joe Xiong <xzh.cmu@gmail.com>
2018-02-13 04:49:52 -0500

    add json "game 3"

--- convert.py
+++ convert.py
@@ -19,7 +19,7 @@ input_json_file = "data/test_data.json"
 #Print out the resulting GameLibrary data using print_game_library(game_library_data) in test_data.py
 ### End Add Code Here ###
 game_library = test_json_utils.make_game_library_from_json(input_json_file)
-print("END!!!!!")
+print(game_library)
 #Part 3
 #Load your custom JSON file
 #Convert JSON data to cc_data
--- test_json_utils.py
+++ test_json_utils.py
@@ -9,23 +9,25 @@ def make_game_library_from_json( json_data ):
     #read the json file
     json_data = open(json_data, 'r').read()
     decoded_games = json.loads(json_data)
-    print('DECODED:', decoded_games)
+    #print('DECODED:', decoded_games)
 
+    # Loop through the json_data
+    # Create a new Game object from the json_data by reading
+    #  title
+    #  year
+    #  platform (which requires reading name and launch_year)
+    # Add that Game object to the game_library
     for decoded_game in decoded_games:
         title = decoded_games[decoded_game]['title']
         year = decoded_games[decoded_game]['Year']
 
         platform_name = decoded_games[decoded_game]['platform']['name']
         platform_launch_year = decoded_games[decoded_game]['platform']['launch year']
-        platform = test_data.Platform(name = platform_name, launch_year=platform_launch_year)
+        platform = test_data.Platform(name=platform_name, launch_year=platform_launch_year)
 
         game_library.games.append(test_data.Game(title=title, year=year, platform=platform))
-    #Loop through the json_data
-        #Create a new Game object from the json_data by reading
-        #  title
-        #  year
-        #  platform (which requires reading name and launch_year)
-        #Add that Game object to the game_library
-    #Return the completed game_library
-    print(game_library)
+
+    #print(game_library)
+
+    # Return the completed game_library
     return game_library
